bbbe0903aad83527ac1547a3dbc2ef3279807c1f
Kimon Hoffmann <Kimon.Hoffmann@lawo.com>
2025-04-01 15:23:09 +0200

    Avoid intermittent flushing of the output stream when formatting flows or grains.

--- lib/src/internal/Flow.cpp
+++ lib/src/internal/Flow.cpp
@@ -2,12 +2,10 @@
 
 #include <cstdint>
 #include <ctime>
-#include <gsl/span>
-#include <iomanip>
-#include <ios>
-#include <mxl/flow.h>
 #include <ostream>
+#include <iomanip>
 #include <uuid.h>
+#include <mxl/flow.h>
 
 
 namespace mxl::lib {
@@ -16,9 +14,11 @@ namespace {
 std::ostream &
 operator<<( std::ostream &os, const timespec &ts )
 {
-    std::time_t sec = ts.tv_sec;
-    struct tm tm;
-    gmtime_r( &sec, &tm );
+    auto sec = ts.tv_sec;
+    auto tm = std::tm{};
+
+    ::gmtime_r( &sec, &tm );
+    // TODO: Altering IOS state as part of the output is problematic
     os << std::put_time( &tm, "%Y-%m-%d %H:%M:%S UTC" ) << " +" << std::setw( 9 ) << std::setfill( '0' ) << ts.tv_nsec << "ns";
     return os;
 }
@@ -27,17 +27,17 @@ operator<<( std::ostream &os, const timespec &ts )
 std::ostream &
 operator<<( std::ostream &os, const Flow &flow )
 {
-    auto span = gsl::span<uint8_t, 16>( const_cast<uint8_t *>( flow.info.id ), sizeof( flow.info.id ) );
-    auto id = uuids::uuid( span );
-    os << "- Flow [" << uuids::to_string( id ) << "]" << std::endl;
-    os << "\tversion           : " << flow.info.version << std::endl;
-    os << "\tstruct size       : " << flow.info.size << std::endl;
-    os << "\tflags             : 0x" << std::setw( 32 ) << std::setfill( '0' ) << std::hex << flow.info.flags << std::dec << std::endl;
-    os << "\thead index        : " << flow.info.headIndex << std::endl;
-    os << "\tgrain rate        : " << flow.info.grainRate.numerator << "/" << flow.info.grainRate.denominator << std::endl;
-    os << "\tgrain count       : " << flow.info.grainCount << std::endl;
-    os << "\tlast write time   : " << flow.info.lastWriteTime << std::endl;
-    os << "\tlast read time    : " << flow.info.lastReadTime << std::endl;
+    auto const span = uuids::span<std::uint8_t, sizeof flow.info.id>{const_cast<std::uint8_t *>( flow.info.id ), sizeof flow.info.id };
+    auto const id = uuids::uuid( span );
+    os  << "- Flow [" << uuids::to_string( id ) << ']' << '\n'
+        << '\t' << std::setw(18) << "version"         << ": " << flow.info.version          << '\n'
+        << '\t' << std::setw(18) << "struct size"     << ": " << flow.info.size             << '\n'
+        << '\t' << std::setw(18) << "flags"           << ": " << std::showbase << std::setw( 32 ) << std::setfill( '0' ) << std::hex << flow.info.flags << std::dec << '\n'
+        << '\t' << std::setw(18) << "head index"      << ": " << flow.info.headIndex        << '\n'
+        << '\t' << std::setw(18) << "grain rate"      << ": " << flow.info.grainRate.numerator << '/' << flow.info.grainRate.denominator << '\n'
+        << '\t' << std::setw(18) << "grain count"     << ": " << flow.info.grainCount       << '\n'
+        << '\t' << std::setw(18) << "last write time" << ": " << flow.info.lastWriteTime    << '\n'
+        << '\t' << std::setw(18) << "last read time"  << ": " << flow.info.lastReadTime     << '\n';
 
     return os;
 }
@@ -45,15 +45,14 @@ operator<<( std::ostream &os, const Flow &flow )
 std::ostream &
 operator<<( std::ostream &os, const Grain &grain )
 {
-    os << "- Grain" << std::endl;
-    os << "\tversion       : " << grain.info.version << std::endl;
-    os << "\tstruct size   : " << grain.info.size << std::endl;
-    os << "\tflags         : 0x" << std::setw( 32 ) << std::setfill( '0' ) << std::hex << grain.info.flags << std::dec << std::endl;
-
-    auto location = ( grain.info.payloadLocation == PAYLOAD_LOCATION_HOST_MEMORY ) ? "host" : "device";
-    os << "\tlocation      : " << location << std::endl;
-    os << "\tdevice        : " << grain.info.deviceIndex << std::endl;
-    os << "\tpayload size  : " << grain.info.grainSize << std::endl;
+    auto const location = ( grain.info.payloadLocation == PAYLOAD_LOCATION_HOST_MEMORY ) ? "host" : "device";
+    os  << "- Grain" << '\n'
+        << '\t' << std::setw(14) << "version"       << ": " << grain.info.version << '\n'
+        << '\t' << std::setw(14) << "struct size"   << ": " << grain.info.size << '\n'
+        << '\t' << std::setw(14) << "flags"         << ": " << std::showbase << std::setw( 32 ) << std::setfill( '0' ) << std::hex << grain.info.flags << std::dec << '\n'
+        << '\t' << std::setw(14) << "location"      << ": " << location << '\n'
+        << '\t' << std::setw(14) << "device"        << ": " << grain.info.deviceIndex << '\n'
+        << '\t' << std::setw(14) << "payload size"  << ": " << grain.info.grainSize << '\n';
 
     return os;
 }
