d28e9673a22eb3fd92d46b7c1d91ce991362ad77
Pedro Ferreira <pedro@bisect.pt>
2025-06-24 12:02:34 +0000

    doc: fix warnings in doc generation for bindings
    Signed-off-by: Pedro Ferreira <pedro@bisect.pt>

--- rust/mxl-sys/src/lib.rs
+++ rust/mxl-sys/src/lib.rs
@@ -2,6 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(missing_docs)]
+#![allow(rustdoc::broken_intra_doc_links)]
+#![allow(rustdoc::invalid_html_tags)]
 // Suppress expected warnings from bindgen-generated code.
 // See https://github.com/rust-lang/rust-bindgen/issues/1651.
 #![allow(deref_nullptr)]
