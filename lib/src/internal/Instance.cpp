f9e5ee3afd78b9e1573122a70d42aae8961ebeec
Pavel Cernohorsky <pavel.cernohorsky@appear.net>
2025-08-07 14:48:02 +0200

    Fix wrong instance being returned from the reader / writer cache
    Signed-off-by: Pavel Cernohorsky <pavel.cernohorsky@appear.net>

--- lib/src/internal/Instance.cpp
+++ lib/src/internal/Instance.cpp
@@ -125,7 +125,7 @@ namespace mxl::lib
         // FIXME: Check result of the from_string operation.
 
         auto const lock = std::lock_guard{_mutex};
-        if (auto const pos = _readers.lower_bound(*id); pos != _readers.end())
+        if (auto const pos = _readers.find(*id); pos != _readers.end())
         {
             auto& v = (*pos).second;
             v.addReference();
@@ -177,7 +177,7 @@ namespace mxl::lib
         // FIXME: Check result of the from_string operation.
 
         auto const lock = std::lock_guard{_mutex};
-        if (auto const pos = _writers.lower_bound(*id); pos != _writers.end())
+        if (auto const pos = _writers.find(*id); pos != _writers.end())
         {
             auto& v = (*pos).second;
             v.addReference();
