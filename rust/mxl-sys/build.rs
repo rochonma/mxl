c90a5d18616e2dc9b26256426a09e0fc3b77a09e
Chris Chan <chris.chan@techex.co.uk>
2025-08-14 15:18:11 +0100

    feat: use `cmake` in `build.rs` for `mxl-sys`
    Signed-off-by: Chris Chan <chris.chan@techex.co.uk>

--- rust/mxl-sys/build.rs
+++ rust/mxl-sys/build.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 
 #[cfg(debug_assertions)]
@@ -27,8 +28,7 @@ fn get_bindgen_specs() -> BindgenSpecs {
             .to_string_lossy()
             .to_string(),
     ];
-    #[cfg(not(feature = "mxl-not-built"))]
-    {
+    if cfg!(not(feature = "mxl-not-built")) {
         let build_dir = repo_root.join("build").join(BUILD_VARIANT);
         let build_version_dir = build_dir
             .join("lib")
@@ -49,6 +49,31 @@ fn main() {
     let bindgen_specs = get_bindgen_specs();
     for include_dir in &bindgen_specs.includes_dirs {
         println!("cargo:include={include_dir}");
+    }
+
+    if cfg!(not(feature = "mxl-not-built")) {
+        // TODO: figure out when this has to be rebuilt
+        let mxl_version_out_path =
+            PathBuf::from(&env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set"))
+                .join("mxl");
+        if !fs::exists(&mxl_version_out_path).expect("Error checking if out path exists") {
+            fs::create_dir(&mxl_version_out_path).expect("Failed to create out path");
+        }
+        let out_path = mxl_version_out_path.join("version.h");
+        println!("cargo:rerun-if-changed={}", out_path.display());
+
+        let dst = cmake::Config::new("../../")
+            .define("BUILD_TESTS", "OFF")
+            .define("BUILD_TOOLS", "OFF")
+            .build();
+
+        let mxl_version_location = dst.join("include").join("mxl").join("version.h");
+        assert!(matches!(std::fs::exists(&mxl_version_location), Ok(true)));
+
+        fs::copy(&mxl_version_location, &out_path).expect("Could copy mxl version");
+
+        println!("cargo:rustc-link-search={}", dst.join("lib64").display());
+        println!("cargo:rustc-link-lib=mxl");
     }
 
     let bindings = bindgen::builder()
