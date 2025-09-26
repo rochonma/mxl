fde4dc6103b1f5b008f7f10895f4c672d8bf77c4
Pavel Cernohorsky <pavel.cernohorsky@appear.net>
2025-09-26 14:38:11 +0200

    Fix mxl-not-built feature
    This feature makes the MXL Rust bindings build skip building the MXL
    library itself and uses the one in the main build directory of MXL
    instead. It got broken after recent changes.
    
    Signed-off-by: Pavel Cernohorsky <pavel.cernohorsky@appear.net>

--- rust/mxl/src/config.rs
+++ rust/mxl/src/config.rs
@@ -5,10 +5,19 @@ use std::str::FromStr;
 
 include!(concat!(env!("OUT_DIR"), "/constants.rs"));
 
+#[cfg(not(feature = "mxl-not-built"))]
 pub fn get_mxl_so_path() -> std::path::PathBuf {
     // The mxl-sys build script ensures that the build directory is in the library path
     // so we can just return the library name here.
     "libmxl.so".into()
+}
+
+#[cfg(feature = "mxl-not-built")]
+pub fn get_mxl_so_path() -> std::path::PathBuf {
+    std::path::PathBuf::from_str(MXL_BUILD_DIR)
+        .expect("build error: 'MXL_BUILD_DIR' is invalid")
+        .join("lib")
+        .join("libmxl.so")
 }
 
 pub fn get_mxl_repo_root() -> std::path::PathBuf {
