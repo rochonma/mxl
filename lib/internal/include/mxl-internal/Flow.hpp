1db5b70471ca1cbabb49030d9a13b869f9361f88
Kimon Hoffmann <Kimon.Hoffmann@lawo.com>
2025-11-06 16:00:04 +0100

    Added proper type specifier to two compile time constants.
    Signed-off-by: Kimon Hoffmann <Kimon.Hoffmann@lawo.com>

--- lib/internal/include/mxl-internal/Flow.hpp
+++ lib/internal/include/mxl-internal/Flow.hpp
@@ -12,10 +12,10 @@
 namespace mxl::lib
 {
     /// The version of the flow data structs in shared memory that we expect and support.
-    constexpr auto FLOW_DATA_VERSION = 1;
+    constexpr auto FLOW_DATA_VERSION = 1U;
 
     /// The version of the grain header structs in shared memory that we expect an support.
-    constexpr auto GRAIN_HEADER_VERSION = 1;
+    constexpr auto GRAIN_HEADER_VERSION = 1U;
 
     ///
     /// Internal Flow structure stored in shared memory
